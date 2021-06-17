a5f7baaedcc476a75a5bcad2057f3b9706e5800d
Sean A Singer <seansinger22@gmail.com>
2021-06-17 13:06:17 -0700

    Fixed a security issue
    forgot to set up logic statement to turn off debug while live.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -138,5 +138,9 @@ LOGIN_URL = 'users:login'
 # Heroku settings
 import os
 import django_heroku
-
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
